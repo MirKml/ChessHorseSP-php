90e012e36fee946f63a6df6e98f33c06e8733d21
Kubelik <koubel@seznam.cz>
2016-09-18 12:53:39 +0200

    improvements: initialize all Squares, no index calculation, no recursion

--- horseSP.php
+++ horseSP.php
@@ -2,9 +2,6 @@
 ini_set("display_errors", true);
 error_reporting(E_ALL);
 
-$startLabel = "a8";
-$endLabel = "h2";
-
 const ROWS = 8;
 const COLUMNS = 8;
 
@@ -14,96 +11,53 @@ class Square
      * square label e.g. "a1", "c5" etc.
      * @var string
      */
-    private $label = "";
-    
-    /**
-     * is this one start position?
-     * @var boo
-     */
-    public $isStart = false;
+    private $label;
 
     /**
-     * is this one end position?
-     * @var boo
+     * index in array which internally represents board
+     * @var int
      */
-    public $isEnd = false;
+    private $boardIndex;
+
+    /**
+     * index of row in real board number in <1..8>
+     * @var int
+     */
+    private $rowIndex;
+
+    /**
+     * index of column in real board number in <1..8>
+     * where 1 corresponds "a"
+     * where 2 corresponds "b"
+     * ...
+     * @var int
+     */
+    private $columnIndex;
 
     /**
      * Square from which it was jumped into current square
      * It's used for path building
      * @var Square
-     */ 
+     */
     public $from;
 
     /**
      * @param string $label
+     * @param int $boardIndex
+     * @param int $rowIndex
+     * @param int $columnIndex
      */
-    public function __construct($label)
+    public function __construct($label, $boardIndex, $rowIndex, $columnIndex)
     {
-        $label = strtolower($label);
-        if (!strlen($label) === 2) {
-            throw new \InvalidArgumentException("label '$label' isn't valid label");
-        }
         $this->label = $label;
+        $this->boardIndex = $boardIndex;
+        $this->rowIndex = $rowIndex;
+        $this->columnIndex = $columnIndex;
     }
 
     public function getLabel()
     {
         return $this->label;
-    }
-
-    /**
-     * gets index of square in board
-     * @return int
-     */
-    public function getBoardIndex()
-    {
-        $columnOffset = ord($this->label[0]) - ord("a");
-        return ((ROWS - $this->label[1]) * COLUMNS) + $columnOffset;
-    }
-    
-    /**
-     * creates square by board index (0, 1, 2 ..), 
-     * initilizes correspondend label: ("a8", "b8")
-     * @return Square
-     */
-    public static function createByIndex($index)
-    {
-        $index++;
-        $row = (int)($index / ROWS);
-        $remainder = $index - $row * COLUMNS;
-        if ($remainder > 0) {
-            $row = ROWS - $row;
-            $column = chr(ord("a") + ($remainder - 1));
-        } else {
-            $row = ROWS - $row + 1;
-            // latest column
-            $column = chr(ord("a") + COLUMNS - 1);
-        }
-
-        $square = new self($column . $row);
-        return $square;
-    }
-
-    /**
-     * gets index of current square row column from range 1 .. ROWS
-     * @return int
-     */
-    private function getRowIndex()
-    {
-        return $this->label[1];
-    }
-
-    /**
-     * gets index of current square column from range 1 .. COLUMNS
-     * for "a" column returns 1, 
-     * for "b" column returns 2
-     * ....
-     * @return int
-     */
-    private function getColumnIndex()
-    {
-        return ord($this->label[0]) - ord("a") + 1;
     }
 
     /**
@@ -113,9 +67,9 @@ class Square
      */
     public function getPossibleJumpsIndexes()
     {
-        $columnIndex = $this->getColumnIndex();
-        $rowIndex = $this->getRowIndex();
-        $currentIndex = $this->getBoardIndex(); 
+        $columnIndex = $this->columnIndex;
+        $rowIndex = $this->rowIndex;
+        $currentIndex = $this->boardIndex;
 
         // right up jump
         if ($columnIndex < COLUMNS - 1 && $rowIndex < ROWS) {
@@ -160,7 +114,7 @@ class Square
 class PathFinder
 {
     /**
-     * @var array
+     * @var Square[]
      */
     private $board;
 
@@ -169,81 +123,103 @@ class PathFinder
      */
     private $start;
 
-    public function __construct(array $board, Square $start)
+    /**
+     * @var Square
+     */
+    private $end;
+
+    /**
+     * @param string $startLabel e.g. "a8"
+     * @param string $endLabel e.g "h1"
+     */
+    public function __construct($startLabel, $endLabel)
     {
-        // add start square into board 
-        $board[$start->getBoardIndex()] = $start;
+        // initialize board ROWS x COLUMNS, as indexed array of Square objects
+        // check the start, end squares
+        $counter = 0;
+        for ($row = ROWS; $row > 0; $row--) {
+            for ($column = 1; $column <= COLUMNS; $column++) {
+                $label = chr(ord("a") + $column - 1) . $row;
+                $square = new Square($label, $counter, $row, $column);
+                if ($label == $startLabel) {
+                    $start = $square;
+                } elseif ($label == $endLabel) {
+                    $end = $square;
+                }
+                $board[$counter++] = $square;
+            }
+        }
+
         $this->board = $board;
         $this->start = $start;
-    }
-    
-    public function find(Square $end)
-    {
-        // add end square into board 
-        $this->board[$end->getBoardIndex()] = $end;
-        $this->makeJumps([$this->start]);
-        return $end;
+        $this->end = $end;
     }
 
     /**
-     * Recursively jumps through all passed positions
-     * and its newly created positions, and so on.
-     * Ends when the end position is reached.
-     * @param Square[] positions
+     * Gets path from start to the end
+     * @return Square[]
      */
-    private function makeJumps(array $positions)
+    public function getPath()
+    {
+        // jump until the end is reached
+        $positions = [$this->start];
+        while (!$this->end->from) {
+            $positions = $this->getJumps($positions);
+        }
+
+        // list of jumps is in reverse order, from end to start
+        $square = $this->end;
+        while ($square !== $this->start) {
+            $path[] = $square;
+            $square = $square->from;
+        }
+        $path[] = $this->start;
+        return array_reverse($path);
+    }
+
+    /**
+     * Gets list of positions for jumps from particular list of positions
+     * If the end square is reached, null is returned
+     * @param Square[]
+     * @return Square[]|null
+     */
+    private function getJumps(array $positions)
     {
         foreach ($positions as $position) {
             foreach ($position->getPossibleJumpsIndexes() as $boardIndex) {
-                // position isn't in board, so create it
-                // it's newly created position for testing in next jump
-                if (!($square = $this->board[$boardIndex])) {
-                    $square = Square::createByIndex($boardIndex);
-                    $this->board[$boardIndex] = $square;
-                    $createdPositions[] = $square;
+                /** @var Square */
+                $jumpedSquare = $this->board[$boardIndex];
+                if ($jumpedSquare->from || $jumpedSquare === $this->start) {
+                    continue;
                 }
-                if (!$square->from) {
-                    $square->from = $position;
-                }
-                if ($square->isEnd) {
+                $jumpedSquare->from = $position;
+                if ($jumpedSquare === $this->end) {
                     return;
                 }
+                $createdPositions[] = $jumpedSquare;
             }
         }
 
         if (!isset($createdPositions)) {
-            throw new Exception("no newly created positions presented!");
+            throw new RuntimeException("no newly created positions presented!");
         }
-        $this->makeJumps($createdPositions);
+        return $createdPositions;
     }
 }
 
 // ========================== main program entry point ==========================
+$finder = new PathFinder("a8", "h2");
+$squarePath = $finder->getPath();
 
-// initialize board ROWS x COLUMNS, as simple indexed array,
-// all indexes has "0" value, which means empty square
-$board = array_fill(0, ROWS * COLUMNS, 0);
-
-$start = new Square($startLabel);
-$start->isStart = true;
-$end = new Square($endLabel);
-$end->isEnd = true;
-
-$finder = new PathFinder($board, $start);
-$end = $finder->find($end);
-
-// list of jumps is in reverse order
-// from end to start 
-$square = $end;
-while (!$square->isStart) {
-    $path[] = $square->getLabel();
-    $square = $square->from;
+// print the path as string
+$path = "";
+foreach ($squarePath as $square) {
+    if ($path) $path .= ", ";
+    $path .= $square->getLabel();
 }
-$path[] = $square->getLabel();
+echo $path . "\n";
 
-echo implode(", ", array_reverse($path)) . "\n";
-
-// results
+// testing results
 // a8, c7, e8, f6, g4, h2
-// a8, c7, e8, f6, g4, h2
-
+// c5, e4, d6, c8
+// b1, d2, f3, h4, g6, f8
